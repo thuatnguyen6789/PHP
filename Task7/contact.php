7a263c5990487a09f66405776a5416cd53d47a5e
nguyenminhhiep26 <73331035+nguyenminhhiep26@users.noreply.github.com>
2021-08-10 13:57:31 +0700

    Add files via upload

---
+++ Task7/contact.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<!-- Website template by freewebsitetemplates.com -->
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Contact - Moonstrosity Custom Shirts Website Template</title>
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+<body>
+<div id="header">
+    <div>
+        <div id="navigation">
+            <div class="infos">
+                <a href="index.php">Cart</a> <a href="index.php">0 items</a>
+            </div>
+            <div>
+                <a href="login.php">Login</a> <a href="register.php">Register</a>
+            </div>
+            <ul id="primary">
+                <li>
+                    <a href="index.php"><span>Home</span></a>
+                </li>
+                <li ">
+                    <a href="about.php"><span>About</span></a>
+                </li>
+                <li>
+                    <a href="men.php"><span>Men</span></a>
+                </li>
+            </ul>
+            <ul id="secondary">
+                <li>
+                    <a href="women.php"><span>Women</span></a>
+                </li>
+                <li>
+                    <a href="blog.php"><span>Blog</span></a>
+                </li>
+                <li class="selected">
+                    <a href="contact.php"><span>Contact</span></a>
+                </li>
+            </ul>
+        </div>
+        <a href="index.php" id="logo"><img src="images/logo.png" alt="LOGO"></a>
+    </div>
+</div>
+<div id="body">
+    <div id="contents">
+        <h1>Contact</h1>
+        <div id="contentsss">
+            <div id="sidebar">
+                <h4>Contact Information</h4>
+                <ul class="contacts">
+                    <li>
+                        <span>Address:</span>
+                        <a href="http://www.freewebsitetemplates.com/misc/contact">
+                           so 8 Ton That Thuyet - My Dinh - Ha Noi
+                        </a>
+                    </li>
+                    <li>
+                        <span>Phone Number:</span>
+                        <a href="http://www.freewebsitetemplates.com/misc/contact" class="num">
+                            1-800-111-1111 or 1-800-999-9999
+                        </a>
+                    </li>
+                    <li>
+                        <span>Fax Number:</span>
+                        <a href="http://www.freewebsitetemplates.com/misc/contact" class="num">
+                            1-800-222-2222
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div id="main">
+                <h4 class="uppercase">Get in touch with us</h4>
+                <form action="index.php" method="post" id="contacts">
+                    <table>
+                        <tbody>
+                        <tr>
+                            <td><label>Name:</label></td>
+                            <td><input type="text" value=""></td>
+                        </tr> <tr>
+                            <td><label>Email Address:</label></td>
+                            <td><input type="text" value=""></td>
+                        </tr> <tr>
+                            <td><label>Phone:</label></td>
+                            <td><input type="text" value=""></td>
+                        </tr> <tr>
+                            <td><label>Message:</label></td>
+                            <td><textarea></textarea></td>
+                        </tr> <tr>
+                            <td colspan="2"><input type="submit" value="" class="button"></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </form>
+            </div>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.096814185285!2d105.78008835075772!3d21.028811885929702!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313455f9bdf0e1c7%3A0x26caee8e7662dd9b!2zRlBUIEFwdGVjaCBIw6AgTuG7mWk!5e0!3m2!1svi!2s!4v1628232359480!5m2!1svi!2s" width="1000" height="400" style="border:0;" allowfullscreen="" loading="lazy"></iframe>        </div>
+    </div>
+</div>
+<div id="footer">
+    <div class="background">
+        <div class="body">
+            <div class="subscribe">
+                <h3>Get Weekly Newsletter</h3>
+                <form action="index.php" method="post">
+                    <input type="text" value="" class="txtfield">
+                    <input type="submit" value="" class="button">
+                </form>
+            </div>
+            <div class="posts">
+                <h3>Latest Post</h3>
+                <p>
+                    Integer sit amet erat at nulla sodales fermentum vel quis mi. <br> Morbi bibendum <a href="#header">...</a> <span>12/08/2021</span>
+                </p>
+            </div>
+            <div class="connect">
+                <h3>Follow Us:</h3>
+                <a href="https://www.facebook.com/hiep.nguyenminh.9887" target="_blank" class="facebook"></a> <a href="http://freewebsitetemplates.com/go/twitter/" target="_blank" class="twitter"></a> <a href="http://freewebsitetemplates.com/go/googleplus/" target="_blank" class="googleplus"></a>
+            </div>
+        </div>
+    </div>
+    <span id="footnote"> <a href="index.php">Moonstrosity Custom Shirts</a> &copy; 2021 | All Rights Reserved.</span>
+</div>
+</body>
+</html>
---
+++ Task7/men.php
@@ -0,0 +1,120 @@
+<?php
+$_SESSION['username'] = "Admin";
+?>
+<!DOCTYPE html>
+<!-- Website template by freewebsitetemplates.com -->
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Mens - Moonstrosity Custom Shirts Website Template</title>
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+<body>
+<div id="header">
+    <div>
+        <div id="navigation">
+            <div class="infos">
+                <a href="index.php">Cart</a> <a href="index.php">0 items</a>
+            </div>
+            <div>
+                <a href="login.php">Login</a> <a href="register.php">Register</a>
+            </div>
+            <ul id="primary">
+                <li>
+                    <a href="index.php"><span>Home</span></a>
+                </li>
+                <li>
+                    <a href="about.php"><span>About</span></a>
+                </li>
+                <li class="selected">
+                    <a href="men.php"><span>Men</span></a>
+                </li>
+            </ul>
+            <ul id="secondary">
+                <li>
+                    <a href="women.php"><span>Women</span></a>
+                </li>
+                <li>
+                    <a href="blog.php"><span>Blog</span></a>
+                </li>
+                <li>
+                    <a href="contact.php"><span>Contact</span></a>
+                </li>
+            </ul>
+        </div>
+        <a href="index.php" id="logo"><img src="images/logo.png" alt="LOGO"></a>
+    </div>
+</div>
+<div id="body">
+    <div id="contents">
+        <h1>Men</h1>
+        <section class="gallery-links">
+
+                <ul id="shirts" class="gallery-container">
+
+                    <?php
+                    include_once 'includes/dhb.inc.php';
+
+                    $sql = "SELECT * FROM gallery ORDER BY orderGallery DESC ;";
+                    $stmt = mysqli_stmt_init($conn);
+                    if (!mysqli_stmt_prepare($stmt,$sql)){
+                        echo "SQL statement failed!";
+                    }else{
+                        mysqli_stmt_execute($stmt);
+                        $result = mysqli_stmt_get_result($stmt);
+                        while ($row = mysqli_fetch_assoc($result)){
+                            echo ' <li>
+                                        <a href="#header"><div style="background-image: url(images/gallery/'.$row["imgFullNameGallery"].');"></div></a><a href="#header" class="button">Add to cart</a>
+                                        <p>
+                                        '.$row["titleGallery"].' <br> <span>&#36;'.$row["descGallery"].'</span>
+                                        </p>
+                                   </li>
+                                ';
+                        }
+                    }
+                    ?>
+                </ul>
+
+        </section>
+    </div>
+    <?php
+    if (isset($_SESSION['username'])){
+        echo '<div class="gallery-upload">
+                            <h2>Upload</h2>
+                            <form action="includes/gallery-upload.inc.php" method="post" enctype="multipart/form-data">
+                                <input type="text" name="filename" placeholder="Name ...">
+                                <input type="text" name="filetitle" placeholder="Title ...">
+                                <input type="text" name="filedesc" placeholder="Salary ...">
+                                <input type="file" name="file">
+                                <button type="submit" name="submit">UPLOAD</button>
+                            </form>
+                        </div>';
+    }
+    ?>
+</div>
+<div id="footer">
+    <div class="background">
+        <div class="body">
+            <div class="subscribe">
+                <h3>Get Weekly Newsletter</h3>
+                <form action="index.php" method="post">
+                    <input type="text" value="" class="txtfield">
+                    <input type="submit" value="" class="button">
+                </form>
+            </div>
+            <div class="posts">
+                <h3>Latest Post</h3>
+                <p>
+                    Integer sit amet erat at nulla sodales fermentum vel quis mi. <br> Morbi bibendum <a href="#header">...</a> <span>12/08/2021</span>
+                </p>
+            </div>
+            <div class="connect">
+                <h3>Follow Us:</h3>
+                <a href="https://www.facebook.com/hiep.nguyenminh.9887" target="_blank" class="facebook"></a> <a href="http://freewebsitetemplates.com/go/twitter/" target="_blank" class="twitter"></a> <a href="http://freewebsitetemplates.com/go/googleplus/" target="_blank" class="googleplus"></a>
+            </div>
+        </div>
+    </div>
+    <span id="footnote"> <a href="index.php">Moonstrosity Custom Shirts</a> &copy; 2021 | All Rights Reserved.</span>
+</div>
+</body>
+</html>
